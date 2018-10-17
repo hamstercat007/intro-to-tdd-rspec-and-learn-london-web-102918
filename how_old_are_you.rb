c504c625673653d186202d67e6e201eb59ac4863
Lisa Mac <lisamac421@gmail.com>
2018-10-17 04:12:16 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -3,4 +3,5 @@ require_relative './current_age_for_birth_year.rb'
 puts "What year were you born?"
 birth_year = gets.to_i 
 
-users_age = current_age_for_birth_year
+users_age = current_age_for_birth_year(birth_year)
+
